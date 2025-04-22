4089b868d6c56efd8981376e1f8d5919e3784863
davvifranco <150635707+davvifranco@users.noreply.github.com>
2025-04-22 09:32:04 -0300

    Update PokemonTerra.java

--- POOProjetoFinal/src/classPokemon/PokemonTerra.java
+++ POOProjetoFinal/src/classPokemon/PokemonTerra.java
@@ -4,15 +4,14 @@ public class PokemonTerra extends Pokemon {
     public String superEfetivo;
     public String fraqueza;
 
-    // Construtor
     public PokemonTerra(String nome, int level, String habilidade, String superEfetivo, String fraqueza, String imagemPath) {
-        super(nome, level, habilidade, imagemPath); // Chama o construtor da classe Pokemon
+        super(nome, level, habilidade, imagemPath); 
         this.superEfetivo = superEfetivo;
         this.fraqueza = fraqueza;
     }
 
     @Override
     public void ataque() {
-        System.out.println(getNome() + " usou ataque de Fogo!");
+        System.out.println(getNome() + " usou ataque de Terra!");
     }
 }
