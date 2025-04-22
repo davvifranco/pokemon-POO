22f95d5e11acd4b8a31ea32c2ae26b8e9222fdf9
davvifranco <150635707+davvifranco@users.noreply.github.com>
2025-04-22 09:30:29 -0300

    Update PokemonEletrico.java

--- POOProjetoFinal/src/classPokemon/PokemonEletrico.java
+++ POOProjetoFinal/src/classPokemon/PokemonEletrico.java
@@ -4,15 +4,14 @@ public class PokemonEletrico extends Pokemon {
     public String superEfetivo;
     public String fraqueza;
 
-    // Construtor
     public PokemonEletrico(String nome, int level, String habilidade, String superEfetivo, String fraqueza, String imagemPath) {
-        super(nome, level, habilidade, imagemPath); // Chama o construtor da classe Pokemon
+        super(nome, level, habilidade, imagemPath);
         this.superEfetivo = superEfetivo;
         this.fraqueza = fraqueza;
     }
 
     @Override
     public void ataque() {
-        System.out.println(getNome() + " usou ataque de Fogo!");
+        System.out.println(getNome() + " usou ataque de Eletricidade!");
     }
 }
